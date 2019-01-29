4a13d593f3af2ab9f1d6fddf56b26a1b567e9064
verma mukesh <vermamukesh67@gmail.com>
2019-01-29 12:35:42 +0530

    Master code setup for app folder arrangements + pod for reachablity + ready to create new branch for further working

---
+++ SPHTechIOSAppTest/SPHTechIOSAppTest/Modules/HomeScreen/ViewModels/HomeViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  HomeViewModel.swift
+//  SPHTechIOSAppTest
+//
+//  Created by Mukesh Verma on 29/01/19.
+//  Copyright © 2019 Mukesh Verma. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ SPHTechIOSAppTest/SPHTechIOSAppTest/Modules/HomeScreen/Views/HomeScreenVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeScreenVC.swift
+//  SPHTechIOSAppTest
+//
+//  Created by Mukesh Verma on 29/01/19.
+//  Copyright © 2019 Mukesh Verma. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
